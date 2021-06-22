ee4f070b8489ac52117682769ab745c5ec3d6610
Anja Laube <mail@anjalaube.com>
2021-06-22 17:17:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Boilerplate
+
+After downloading, remove all js files from src-folder!
+# Thinkmoto-task1
+# Thinkmoto-task1
---
+++ src/index.js
@@ -0,0 +1,15 @@
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 1,
+  dots: '#dots',
+  draggable: true,
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
+
+window.addEventListener('load', function () {
+  new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+  })
+})
