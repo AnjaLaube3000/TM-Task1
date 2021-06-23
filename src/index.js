65ef60b6f25ae0c481160faa89947c51b3116709
Anja Laube <mail@anjalaube.com>
2021-06-23 17:16:51 +0200

    two sliders work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import Splide from '@splidejs/splide'
-// new Splide('.splide').mount();
 
 document.addEventListener( 'DOMContentLoaded', function () {
-  // new Splide('.splide').mount();
-  new Splide('.splide', {
+  slider_one()
+  slider_two()
+  // global_carousel__ctrl();
+})
+
+function slider_one() {
+  const one = new Splide('#one', {
     type:'fade',
     perPage: 1,
     padding:0,
@@ -17,8 +21,44 @@ document.addEventListener( 'DOMContentLoaded', function () {
     // easing: 'cubic-bezier(.645,.045,.335,1)',
     arrows: false,
     pagination: false
-
   }).mount()
+}
+
+function slider_two() {
+  const one = new Splide('#two', {
+    type: 'fade',
+    perPage: 1,
+    padding: 0,
+    gap: 0,
+    rewind: true,
+    // width: '1000px',
+    height: '500px',
+    speed: '0.8s',
+    autoplay: true,
+    easing: 'ease',
+    // easing: 'cubic-bezier(.645,.045,.335,1)',
+    arrows: false,
+    pagination: false
+  }).mount()
+}
 
 
-})
+// function global_carousel__ctrl() {
+//   var elms = document.getElementsByClassName("splide");
+//   for (var i = 0; i < elms.length; i++) {
+//     new Splide(elms[i], {
+//       perPage: 1,
+//       padding: 0,
+//       gap: 0,
+//       rewind: true,
+//       // width: '1000px',
+//       height: '500px',
+//       speed: '0.8s',
+//       autoplay: true,
+//       easing: 'ease',
+//       // easing: 'cubic-bezier(.645,.045,.335,1)',
+//       arrows: false,
+//       pagination: false
+//     }).mount();
+//   }
+// }
