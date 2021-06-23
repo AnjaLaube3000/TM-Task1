b710c6d422498a924cab9d386c142c0395616c03
Anja Laube <mail@anjalaube.com>
2021-06-23 13:04:57 +0200

    first slider in place

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-new Glider(document.querySelector('.glider'), {
-  slidesToShow: 1,
-  dots: '#dots',
-  draggable: true,
-  arrows: {
-    prev: '.glider-prev',
-    next: '.glider-next'
-  }
-});
+import Splide from '@splidejs/splide'
+// new Splide('.splide').mount();
 
-window.addEventListener('load', function () {
-  new Glider(document.querySelector('.glider'), {
-    slidesToShow: 1,
-  })
+document.addEventListener( 'DOMContentLoaded', function () {
+  // new Splide('.splide').mount();
+  new Splide('.splide', {
+    type:'fade',
+    perPage: 1,
+    padding:0,
+    gap: 0,
+    rewind: true,
+    // width: '1000px',
+    height: '500px',
+    speed: '0.8s',
+    autoplay: true,
+    easing: 'ease',
+    // easing: 'cubic-bezier(.645,.045,.335,1)',
+    arrows: false,
+    pagination: false
+
+  }).mount()
 })
