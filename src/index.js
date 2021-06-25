71ad4a462c3c679d2c4b36e404a782408b8f6743
Anja Laube <mail@anjalaube.com>
2021-06-25 14:14:05 +0200

    arrow function doesnt work, solving other problems first

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Splide from '@splidejs/splide'
 
 document.addEventListener( 'DOMContentLoaded', function () {
   new Splide('#one', {
-    type:'fade',
+    type:'slide',
     perPage: 1,
     // padding:0,
     // gap: 0,
@@ -17,12 +17,12 @@ document.addEventListener( 'DOMContentLoaded', function () {
     // flickPower: 500,
     // flickMaxPages: 1,
     arrows: true,
-    arrowPath: 'public/images/yellow-arrow-left.svg',
+    // arrowPath: 'public/images/yellow-arrow-left.svg',
     pagination: false
   }).mount().sync(true),
 
   new Splide ('#two', {
-      type: 'fade',
+      type: 'slide',
       perPage: 1,
       // padding: 0,
       // gap: 0,
@@ -37,20 +37,20 @@ document.addEventListener( 'DOMContentLoaded', function () {
       // flickPower: 500,
       // flickMaxPages: 1,
       arrows: true,
-    arrowPath: 'public/images/yellow-arrow-right.svg',
+      // arrowPath: 'public/images/yellow-arrow-right.svg',
       pagination: false
     }).mount().sync(true)
 
 })
 
 
-// //event listener for arrows
-let arrow = document.querySelector('button')
-const onMouseMove = (e) => {
-  arrow.style.left = e.pageX + 'px'
-  arrow.style.top = e.pageY + 'px'
-}
-document.addEventListener('mousemove', onMouseMove)
+// // //event listener for arrows
+// let arrow = document.querySelector('button')
+// const onMouseMove = (e) => {
+//   arrow.style.left = e.pageX + 'px'
+//   arrow.style.top = e.pageY + 'px'
+// }
+// document.addEventListener('mousemove', onMouseMove)
 
 
 // let circle = document.getElementById('circle');
